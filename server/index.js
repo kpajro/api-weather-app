ca88764184e45461b95c796dc110f7768041447e
ihavesandineyes <fudempror@gmail.com>
2024-08-12 19:53:46 +0200

    more changes for deployment

--- server/index.js
+++ server/index.js
@@ -17,12 +17,12 @@ app.use(cors({
 
 
 const pool = mysql.createPool({
-    host: "b53zwczmaolljw37hn7l-mysql.services.clever-cloud.com",
-    user: "upcdru6vijrwied9",
-    password: "KhgSGhgxwhQ2Njlaflir",
-    database: "b53zwczmaolljw37hn7l",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10, 
+    connectionLimit: 10,
     queueLimit: 0
 });
 
